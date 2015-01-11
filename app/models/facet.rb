2c3a058ae1d3c08c0557bf3e0f64dd008075d62f
catarak <ctarakajian@gmail.com>
2015-01-11 15:26:43 -0500

    add facet model

--- app/models/facet.rb
+++ app/models/facet.rb
@@ -1,4 +1,22 @@
 class Facet
-  def initialize(vertices, )
+  attr_accessor :vertices, :normal
+  def initialize(vertices, normal)
+    @vertices = vertices
+    @normal = normal
+    #throw an exception if normal is not a vector
+    #and if vertces is not an array of length three of vertices
+  end
+
+  def ==(facet)
+    facet.normal == self.normal && vertices_equal?(facet.vertices)
+  end
+
+  private
+
+    def vertices_equal?(vertices)
+      vertices.all? do |vertex|
+        self.vertices.include?(vertex)
+      end
+    end
 
 end
