ef8eb1acae018b82924002a4cea11cc2f198af41
catarak <ctarakajian@gmail.com>
2015-01-11 16:29:28 -0500

    solid teste

--- app/models/solid.rb
+++ app/models/solid.rb
@@ -1,3 +1,16 @@
 class Solid
+  attr_accessor :name
+  attr_reader :facets, :vertices
+  def initialize(name)
+    @name = name
+    @vertices = []
+    @facets = []
+  end
 
+  def add_facet(facet)
+    self.facets << facet unless self.facets.include?(facet)
+    facet.vertices.each do |vertex|
+      self.vertices << vertex unless self.vertices.include?(vertex)
+    end
+  end
 end
